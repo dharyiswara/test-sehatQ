fece1dee0959c2cbbf26ae5e9040d4647f2fde31
Dary Hilmy Iswara <dary.iswara@redkendi.com>
2019-10-02 21:18:47 +0700

    initial commit

---
+++ app/src/main/java/com/dharyiswara/sehatqtest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dharyiswara.sehatqtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
