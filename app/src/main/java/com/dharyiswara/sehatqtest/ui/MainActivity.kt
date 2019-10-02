6d38a2d81d5d5533d52c6b7a20901640954c4a2a
Dary Hilmy Iswara <dary.iswara@redkendi.com>
2019-10-02 23:10:56 +0700

    update activity base class

---
+++ app/src/main/java/com/dharyiswara/sehatqtest/ui/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.dharyiswara.sehatqtest.ui
+
+import com.dharyiswara.sehatqtest.R
+import com.dharyiswara.sehatqtest.base.BaseActivity
+
+class MainActivity : BaseActivity() {
+
+    override fun getLayoutResId(): Int = R.layout.activity_main
+
+}
